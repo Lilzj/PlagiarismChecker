3bc767490bd72e1a568a928c4355d1696497bad2
Gbogodor Joshua <joshuagbogodor@gmail.com>
2022-01-11 06:08:29 +0100

    added gitignore

--- PlagiarismChecker.Data/PlagiarismContext.cs
+++ PlagiarismChecker.Data/PlagiarismContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using PlagiarismChecker.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,15 @@ using System.Threading.Tasks;
 
 namespace PlagiarismChecker.Data
 {
-    class PlagiarismContext
+    class PlagiarismContext : DbContext
     {
+        public PlagiarismContext(DbContextOptions<PlagiarismContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Documents> Documents { get; set; }
+        public DbSet<ComparisonHistory> ComparisonHistories { get; set; }
     }
 }
